1f7ac6db509332b1bdddd6921b6e4a5a66697ec1
Kamal Kumar Mandal <43649282+kamalkumarmandal@users.noreply.github.com>
2022-08-05 09:46:45 +0530

    Update SubwayRun.bat

--- Subway Jar files/SubwayRun.bat
+++ Subway Jar files/SubwayRun.bat
@@ -1,10 +1,4 @@
 @echo off
 cd desktop
-Console.BackgroundColor = ConsoleColor.Green;
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.Clear();
-//green on green
-Console.WriteLine("yo");
-Console.ReadLine();
 java -jar subwayPro1.jar
 pause
